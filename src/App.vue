e3c9e076116c4926d5bf0104c8cb38948fed3971
Guilleveron <guilleveron.gvf.gv@gmail.com>
2021-10-29 17:20:43 -0300

    buscador añadido

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
                     <img src="@/assets/Logo.svg" alt="Moniciones Litúrgicas" title="Moniciones Litúrgicas" class="logo">
                     Moniciones Litúrgicas
                 </a>
-                <button id="navbar-toggler-ocultar-buscador" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button id="navbar-toggler-ocultar-buscador" v-on:click="ocultar_buscador()" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -30,14 +30,15 @@
                             </ul>
                         </li>
                     </ul>
-                    <form method="get" action="/buscar" class="d-flex" id="ctn-bars-search" target="_blank">
-                        <input class="form-control me-1" autocomplete="off" type="search" id="inputSearch" name="monición" placeholder="Día, Semana, Solemnidad o Festividad">
+                    <form method="get" action="/buscar" class="d-flex" id="ctn-bars-search" v-on:click="mostrar_buscador()" target="_blank">
+                        <input class="form-control me-1" autocomplete="off" type="search" id="inputSearch" @keyup="buscador_interno()" name="monición" placeholder="Día, Semana, Solemnidad o Festividad">
                         <button class="btn btn-outline-primary" type="submit" value="Buscar">Buscar<i class="fas fa-search" ></i></button>
                     </form>
                 </div>
             </div> 
         </nav>
-        <div class="principal">
+        <ul id="box-search" v-on:click="ocultar_buscador()"></ul>
+        <div class="principal" v-on:click="ocultar_buscador()">
             <router-view/>  
             <Pie/>
         </div>       
@@ -61,10 +62,71 @@ export default {
     }  
   },
   methods:{
-      
+      async cargarMonicionesBuscador(){
+        await fetch(`https://api-moniciones-liturgicas.herokuapp.com/api/moniciones/`)
+            .then(respuesta => {return respuesta.json()})
+            .then(monicion => {
+                let monicionAMostrar = document.querySelector('#box-search');
+                //console.log(monicion);
+                monicion.forEach(monicion => {
+                    const row = document.createElement("li");
+                row.innerHTML += //html
+                `
+                    <a href="/monicion?titulo=${monicion.titulo}&dia=${monicion.dia}&semana=${monicion.semana}&ciclo=${monicion.ciclo}&tiempo=${monicion.tiempo}&id=${monicion.id}" target="_blank">
+                        <i class="fas fa-search"></i>
+                        ${monicion.titulo} ${monicion.dia} ${monicion.semana} - Tiempo ${monicion.tiempo} - Ciclo ${monicion.ciclo} <i class="fas fa-angle-double-right">
+                        </i>
+                        <div class="d-none">${monicion.titulo} ${monicion.dia} ${monicion.semana} tiempo ${monicion.tiempo} ciclo ${monicion.ciclo} ${monicion.entrada}
+                        </div>
+
+                    </a>
+                `;
+                monicionAMostrar.appendChild(row);
+                    })
+                })
+            .catch(error => console.log("Hubo un Error: " + error.message))
+    },
+    //Funcion para mostrar el buscador
+    mostrar_buscador(){
+        let inputSearch = document.getElementById("inputSearch");
+        let box_search = document.getElementById("box-search");
+        inputSearch.focus();
+        if (inputSearch.value === ""){
+            box_search.style.display = "none";
+        }
+    },
+    //Funcion para ocultar el buscador
+    ocultar_buscador(){
+        let inputSearch = document.getElementById("inputSearch");
+        let box_search = document.getElementById("box-search");
+        inputSearch.value = "";
+        box_search.style.display = "none";
+    },
+    //Creando filtrado de busqueda
+    buscador_interno(){
+        let inputSearch = document.getElementById("inputSearch");
+        let box_search = document.getElementById("box-search");
+        let filter = inputSearch.value.toUpperCase();
+        let li = box_search.getElementsByTagName("li");
+        //Recorriendo elementos a filtrar mediante los "li"
+        for (let i = 0; i < li.length; i++){
+            let a = li[i].getElementsByTagName("a")[0];
+            let textValue = a.textContent || a.innerText;
+            if(textValue.toUpperCase().indexOf(filter) > -1){
+                li[i].style.display = "";
+                box_search.style.display = "block";
+                if (inputSearch.value === ""){
+                box_search.style.display = "none";
+                }
+            }else{
+                li[i].style.display = "none";
+            }
+        }
+    }
   },
   created: function(){
-
+        this.cargarMonicionesBuscador();
+        
   }
 }
 
