4703f24db91c6aa3af6e05157219b47b4b9a77ef
Guilleveron <guilleveron.gvf.gv@gmail.com>
2021-11-16 09:59:49 -0300

    correccion de google adsense

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -5,7 +5,6 @@
     <MonicionEspecial/>
     <MonicionesDominicales/>
     <MonicionMariana/>
-    <GoogleAdsense style="display:block" ad-client="ca-pub-6129079724757336" ad-slot="6840732830"></GoogleAdsense>
   </div>
 </template>
 
@@ -16,7 +15,6 @@ import Carousel from '@/components/CompCarousel'
 import MonicionEspecial from '@/components/CompMonicionEspecial'
 import MonicionesDominicales from '@/components/CompMonicionesDominicales'
 import MonicionMariana from '@/components/CompMonicionMariana'
-import GoogleAdsense from '@/components/CompGoogleAdsense'
 
 import { mapState, mapActions } from "vuex";
 
@@ -26,8 +24,7 @@ export default {
     Carousel,
     MonicionEspecial,
     MonicionesDominicales,
-    MonicionMariana,
-    GoogleAdsense
+    MonicionMariana
   },
   computed:{
     //...mapState([''])
